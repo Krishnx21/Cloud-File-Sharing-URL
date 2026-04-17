74d7a1a162526bfbb2e00962d2e7aee9d06ad66f
Krishna Kumar <krishnakumarsharma8077@gmail.com>
2026-04-17 14:28:37 +0530

    router and controller

---
+++ src/db/index2.js
@@ -0,0 +1,17 @@
+// Basic MongoDB connection for ES module
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("MongoDB connected successfully");
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
