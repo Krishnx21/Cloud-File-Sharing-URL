6ee52d16598857235d424822ba4083195225cf75
Krishna Kumar <krishnakumarsharma8077@gmail.com>
2026-05-27 17:19:27 +0530

    multer

--- src/controllers/user.controler.js
+++ src/controllers/user.controler.js
@@ -3,6 +3,7 @@
  
 export const userController = (req, res) => {
 
+
   
 //1. Get data from frontend (req.body)
 //2. Validate fields
@@ -17,7 +18,14 @@ export const userController = (req, res) => {
 // 6. Check if user created successfully]
 // 7. Send response
 
-  const {usename,email,password}=req.body
+// 1. Get file from frontend
+// 2. Validate file
+// 3. Upload to cloud
+// 4. Save file URL in DB
+// 5. Generate shareable link
+// 6. Send response
+
+  const {username,email,password}=req.body
   console.log("user",username);
   
 
