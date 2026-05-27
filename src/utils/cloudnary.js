866dc0d5ba42cbd9c3c5ed44c2aa2f0ccafc72ab
Krishna Kumar <krishnakumarsharma8077@gmail.com>
2026-05-27 19:12:23 +0530

    cloudnaryjs

--- src/utils/cloudnary.js
+++ src/utils/cloudnary.js
@@ -1,45 +1,42 @@
-import {v2 as cloudinary} from 'cloudinary';
-import fs from 'fs';
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
 
+// Cloudinary configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
+// Upload function
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
 
-(async function() {
+    // Check if file exists
+    if (!localFilePath) return null;
 
-    // Configuration
-    cloudinary.config({ 
-        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-        api_key: process.env.CLOUDINARY_API_KEY, 
-        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
-    });
-    
-    // Upload an image
-     const uploadResult = await cloudinary.uploader
-       .upload(
-           'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
-               public_id: 'shoes',
-           }
-       )
-       .catch((error) => {
-           console.log(error);
-       });
-    
-    console.log(uploadResult);
-    
-    // Optimize delivery by resizing and applying auto-format and auto-quality
-    const optimizeUrl = cloudinary.url('shoes', {
-        fetch_format: 'auto',
-        quality: 'auto'
-    });
-    
-    console.log(optimizeUrl);
-    
-    // Transform the image: auto-crop to square aspect_ratio
-    const autoCropUrl = cloudinary.url('shoes', {
-        crop: 'auto',
-        gravity: 'auto',
-        width: 500,
-        height: 500,
-    });
-    
-    console.log(autoCropUrl);    
-})();
+    // Upload file to cloudinary
+    const response = await cloudinary.uploader.upload(
+      localFilePath,
+      {
+        resource_type: "auto"
+      }
+    );
+
+    console.log("File uploaded successfully on Cloudinary");
+
+    // Return uploaded file response
+    return response;
+
+  } catch (error) {
+
+    // Remove local file if upload fails
+    fs.unlinkSync(localFilePath);
+
+    console.log("Cloudinary upload failed", error);
+
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
