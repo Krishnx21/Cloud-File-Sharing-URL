e74ed4bcc94a095a40dbea58ded7f8f1a3dce47a
Krishna Kumar <krishnakumarsharma8077@gmail.com>
2026-05-31 10:05:11 +0530

    file model

---
+++ src/modules/file.model.js
@@ -0,0 +1,39 @@
+
+
+const fileSchema = new mongoose.Schema({
+  // File name as uploaded by user.
+  filename: {
+    type: String,
+    required: true
+  },
+  // URL of the file stored in Cloudinary.
+  url: {
+    type: String,
+    required: true
+  },
+  // Cloudinary public id for the file, used for deletion.
+  publicId: {
+    type: String,
+    required: true
+  },
+  // Reference to the user who uploaded the file.
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  size: {
+    type: Number,
+    required: true
+  },
+  mimetype: {
+    type: String,
+    required: true  
+  },
+  expiresAt: {
+    type: Date,
+    required: true
+  },
+}, { timestamps: true });
+
+export const File = mongoose.model("File", fileSchema);
